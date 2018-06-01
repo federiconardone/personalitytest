ed19138aefbd553a7faab4b1c92a88c6922e4c81
Federico <federico.nardone@gmail.com>
2018-06-01 10:56:10 +0200

    Eclipse test

--- src/pckApplication/Intro.java
+++ src/pckApplication/Intro.java
@@ -11,7 +11,7 @@ import pckUtilities.StringLocalizer;
 
 /*
  * It is the starting point of the application; it contains the method
- * for the language selection
+ * for the language selection test
  */
 public class Intro {
 	
